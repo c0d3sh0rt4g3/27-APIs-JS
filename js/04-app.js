8fd1b2864ff42bd058b763dd452e5f2a064a2cfb
out0f4mm0 <eriking2004@gmail.com>
2023-11-08 09:49:21 +0100

    Done example 4

---
+++ js/04-app.js
@@ -0,0 +1,8 @@
+const openBTN = document.querySelector("#abrir-pantalla-completa")
+const exitBTN = document.querySelector("#salir-pantalla-completa")
+
+const enterFullScreen = () =>{document.documentElement.requestFullscreen()}
+const exitFullScreen = () => {document.exitFullscreen()}
+
+openBTN.addEventListener("click", enterFullScreen)
+exitBTN.addEventListener("click", exitFullScreen)
